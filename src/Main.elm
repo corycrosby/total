349b82833eaf02f50d1e74f107458f32ffa0d5e2
Cory Crosby <cory@secondspectrum.com>
2018-06-28 22:19:10 -0700

    add a teams/units page

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,9 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, program, text)
+import Html.Styled as Html exposing (Html, a, div, program, text, toUnstyled)
+import Html.Styled.Attributes exposing (css, href)
 import Navigation exposing (Location)
+import Style exposing (..)
 import UrlParser as Url exposing ((</>), parsePath, top)
 
 
@@ -27,6 +29,12 @@ init location =
         Home ->
             ( initialModel currentPage, Cmd.none )
 
+        Teams ->
+            ( initialModel currentPage, Cmd.none )
+
+        Units ->
+            ( initialModel currentPage, Cmd.none )
+
         PageNotFound ->
             ( initialModel currentPage, Cmd.none )
 
@@ -45,6 +53,8 @@ parseUrl : Url.Parser (Page -> a) a
 parseUrl =
     Url.oneOf
         [ Url.map Home top
+        , Url.map Teams (Url.s "teams")
+        , Url.map Units (Url.s "units")
         ]
 
 
@@ -59,6 +69,8 @@ type Msg
 
 type Page
     = Home
+    | Teams
+    | Units
     | PageNotFound
 
 
@@ -73,9 +85,25 @@ view model =
             div []
                 [ text "Home" ]
 
+        Teams ->
+            viewTeams
+
+        Units ->
+            div []
+                [ text "Units" ]
+
         PageNotFound ->
             div []
                 [ text "Page Not Found" ]
+
+
+viewTeams : Html Msg
+viewTeams =
+    div [ css [ Style.teamsContainer ] ]
+        [ div [] [ text "teams" ]
+        , a [ href "/teams/highelf" ] [ text "highelf" ]
+        , a [ href "/teams/orc" ] [ text "orc" ]
+        ]
 
 
 
@@ -109,7 +137,7 @@ main : Program Never Model Msg
 main =
     Navigation.program OnLocationChange
         { init = init
-        , view = view
+        , view = view >> toUnstyled
         , update = update
         , subscriptions = subscriptions
         }
